2764b4fb10d37bdb3bff4b1ce78db9bcae22edd2
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-06-20 23:22:21 +0100

    build tested all collections mapped

--- app/[locale]/family-album/page.tsx
+++ app/[locale]/family-album/page.tsx
@@ -3,14 +3,20 @@ import React from "react";
 import ContentFamily from "./content-family";
 import { getData } from "@/functions/api/getData";
 
-import photoMapper from "@/functions/transformers/photoMapper";
+
 import { Item, PhotoFieldsRaw } from "@/app/interfaces";
 import ContentContainer from "@/components/custom beta components/ContentContainer";
+import photoMapper from "@/functions/transformers/photoRawToLightBox";
+import { getIdByDisplayName } from "@/functions/utils/findCollectionId";
 
 export default async function page() {
   const dataPhotos = await getData("64aed077835b06888cf9e4c0");
+  const peopleCollectionID = getIdByDisplayName("People");
+  const programmeCollectionID = getIdByDisplayName("Programmes");
+  const programmesRawData = await getData(programmeCollectionID);
+  const peopleRawData = await getData(peopleCollectionID);
   const dataPhotosRaw: Item<PhotoFieldsRaw>[] = dataPhotos.items;
-  const photos = dataPhotosRaw.map((item) => photoMapper(item));
+  const photos = dataPhotosRaw.map((item) => photoMapper(item, programmesRawData.items, peopleRawData.items));
 
   return (
     <div>
