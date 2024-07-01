427d648000e5f799dbdce3d0e367429f4980c5e9
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-07-01 12:32:54 +0100

    Conditional Display for TableCJ Rows

--- components/CJ-components/components-CJ/basic components/ButtonCJ.tsx
+++ components/CJ-components/components-CJ/basic components/ButtonCJ.tsx
@@ -10,7 +10,7 @@ interface ButtonCJProps {
 
 const ButtonCJ: React.FC<ButtonCJProps> = ({ href, text }) => {
   return (
-    <a href={href} className="inline-block">
+    <a href={href} target='_blank' className="inline-block">
       <button className="text-tiny rounded-lg border-2 border-blue-500 bg-transparent px-8 py-2 font-bold uppercase text-blue-500 transition duration-150 ease-in-out hover:border-orange-500 hover:text-orange-500">
         {text}
       </button>
