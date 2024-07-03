0ff765c0d26e54100a7ae223dfe9095d21d04a07
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-07-03 10:20:38 +0100

    03 july all comits with related coillections

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['img.daisyui.com','uploads-ssl.webflow.com','img.youtube.com'],
+        domains: ['img.daisyui.com','uploads-ssl.webflow.com','img.youtube.com'],  unoptimized: true,
       },
 }
 
