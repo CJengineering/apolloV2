0b0c9bf3fad713518a2f69dbeed00c85d02a235a
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-07-16 11:23:55 +0100

    filter for news posts

--- app/[locale]/news/post-context.tsx
+++ app/[locale]/news/post-context.tsx
@@ -1,16 +1,13 @@
-'use client';
+"use client";
 
-
-import { PostFieldsCleaned } from '@/app/interfaces';
+import { PostFieldsCleaned } from "@/app/interfaces";
 // src/context/PostContext.tsx
-import React, { createContext, useContext, useMemo, useState } from 'react';
+import React, { createContext, useContext, useMemo, useState } from "react";
 
 interface RelatedCollection {
   id: string;
   name: string;
 }
-
-
 
 interface PostContextProps {
   programmes: RelatedCollection[];
@@ -19,6 +16,8 @@ interface PostContextProps {
   filteredPosts: PostFieldsCleaned[];
   setProgrammeFilter: (programmes: RelatedCollection[]) => void;
   setPeopleFilter: (people: RelatedCollection[]) => void;
+  postQuery: string;
+  setPostQuery: (query: string) => void;
 }
 
 const PostContext = createContext<PostContextProps | undefined>(undefined);
@@ -26,7 +25,7 @@ const PostContext = createContext<PostContextProps | undefined>(undefined);
 export const usePostContext = () => {
   const context = useContext(PostContext);
   if (!context) {
-    throw new Error('usePostContext must be used within a PostProvider');
+    throw new Error("usePostContext must be used within a PostProvider");
   }
   return context;
 };
@@ -36,25 +35,53 @@ interface PostProviderProps {
   programmes: RelatedCollection[];
   people: RelatedCollection[];
   postsClean: PostFieldsCleaned[];
-
 }
 
-export const PostProvider: React.FC<PostProviderProps> = ({ children, programmes, people,postsClean }) => {
-  const [selectedProgrammes, setSelectedProgrammes] = useState<RelatedCollection[]>([]);
+export const PostProvider: React.FC<PostProviderProps> = ({
+  children,
+  programmes,
+  people,
+  postsClean,
+}) => {
+  const [selectedProgrammes, setSelectedProgrammes] = useState<
+    RelatedCollection[]
+  >([]);
   const [selectedPeople, setSelectedPeople] = useState<RelatedCollection[]>([]);
+  const [postQuery, setPostQuery] = useState("");
 
   const filteredPosts = useMemo(() => {
-    if (selectedProgrammes.length === 0 && selectedPeople.length === 0) {
-      return postsClean;
+    let filtered = postsClean; // Start with the clean list of posts
+
+    if (selectedProgrammes.length > 0 || selectedPeople.length > 0) {
+      filtered = filtered.filter((post) => {
+        const matchesProgramme =
+          selectedProgrammes.length > 0
+            ? selectedProgrammes.some(
+                (programme) => post.programme.shortname === programme.name
+              )
+            : false;
+        const matchesPeople =
+          selectedPeople.length > 0
+            ? selectedPeople.some((selectedPerson) =>
+                post.people.some(
+                  (person) => person.name === selectedPerson.name
+                )
+              )
+            : false;
+
+        return matchesProgramme || matchesPeople;
+      });
     }
-    return postsClean.filter(post => {
-      const matchesProgramme = selectedProgrammes.length > 0 ? selectedProgrammes.some(programme => post.programme.shortname === programme.name): false;
-      const matchesPeople = selectedPeople.length > 0 ? selectedPeople.some(selectedPerson =>
-        post.people.some(person => person.name === selectedPerson.name)
-      ) : false;
-      return matchesProgramme || matchesPeople;
-    });
-  }, [postsClean, selectedProgrammes, selectedPeople]);
+
+    // Further filter by the post query if it's not empty
+    if (postQuery.trim() !== "") {
+      filtered = filtered.filter((post) =>
+        post.name.toLowerCase().includes(postQuery.toLowerCase())
+      );
+    }
+
+    return filtered;
+  }, [postsClean, selectedProgrammes, selectedPeople, postQuery]);
 
   const setProgrammeFilter = (programmes: RelatedCollection[]) => {
     setSelectedProgrammes(programmes);
@@ -65,9 +92,19 @@ export const PostProvider: React.FC<PostProviderProps> = ({ children, programmes
   };
 
   return (
-    <PostContext.Provider value={{ programmes, people, postsClean, filteredPosts, setProgrammeFilter, setPeopleFilter }}>
+    <PostContext.Provider
+      value={{
+        programmes,
+        people,
+        postsClean,
+        filteredPosts,
+        setProgrammeFilter,
+        setPeopleFilter,
+        postQuery,
+        setPostQuery,
+      }}
+    >
       {children}
     </PostContext.Provider>
   );
 };
-
