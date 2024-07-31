9ecbf8e47b93d8000e35a6d797fa55c5ef0b0e88
Nathaniel Daudrich <nathaniel@communityjameel.org>
2024-07-31 12:22:21 +0200

    quick updates

--- components/custom beta components/MansoryGrid.tsx
+++ components/custom beta components/MansoryGrid.tsx
@@ -10,7 +10,7 @@ interface MasonryGridProps {
 
 const MasonryGrid: React.FC<MasonryGridProps> = ({ images, onImageClick }) => {
   return (
-    <div className="columns-1 sm:columns-1 md:columns-3 gap-4">
+    <div className="w-full columns-1 sm:columns-1 md:columns-5 gap-4">
       {images.map((image, index) => (
         <div key={index} className="mb-4 break-inside-avoid" onClick={() => onImageClick(index)}>
           <Image src={image.src} width={500} loading='lazy' height = {400} alt={image.alt} className="w-full h-auto cursor-pointer" />
