67001695442d0a92fb945cfd601f5d968debd72f
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-09-11 16:18:15 +0100

    caroussel ok

--- components/CJ-components/components-CJ/basic components/CarousselForComponents.tsx
+++ components/CJ-components/components-CJ/basic components/CarousselForComponents.tsx
@@ -28,47 +28,37 @@ const CarouselForComponent = ({ children }: CarouselProps) => {
   }
 
   return (
-    <div className="relative mx-auto 2xl:w-[1200px] xl:w-[900px] lg:w-[700px] md:w-[500px] ">
-    {/* Left Arrow: Hidden on mobile */}
-    <div
-      className="absolute left-2 top-1/2 z-10 -translate-y-1/2 transform hidden md:block"
-      style={{ left: '-2rem' }}
-    >
-      <button
-        onClick={scrollLeft}
-        className="rounded-full bg-gray-700 p-2 text-white"
-      >
-        &#10094;
-      </button>
-    </div>
-
-    {/* Right Arrow: Hidden on mobile */}
-    <div
-      className="absolute top-1/2 z-10 -translate-y-1/2 transform hidden md:block"
-      style={{ right: '-2rem' }}
-    >
-      <button
-        onClick={scrollRight}
-        className="rounded-full bg-gray-700 p-2 text-white"
-      >
-        &#10095;
-      </button>
-    </div>
-
-    <div
-      ref={carouselRef}
-      className="carousel flex carousel-center bg-neutral rounded-box custom-scrollbar gap-4 overflow-x-auto p-4"
-    >
-      {children.map((child, index) => (
-        <div
-          key={index}
-          className="2xl:min-w-[400px] xl:min-w-[350px] lg:min-w-[300px] md:min-w-[250px] min-w-[300px] bg-white p-4 rounded shadow-lg"
+    <div className="relative  2xl:w-full ">
+      {/* Arrows Container: Positioned at the top center */}
+      <div className="top-0 left-0 right-0 z-10 flex justify-end space-x-4 mt-4">
+        <button
+          onClick={scrollLeft}
+          className="rounded-full  p-2 text-gray-500"
         >
-          {child}
-        </div>
-      ))}
+          &#10094;
+        </button>
+        <button
+          onClick={scrollRight}
+          className="rounded-full  p-2 text-gray-500"
+        >
+          &#10095;
+        </button>
+      </div>
+
+      <div
+        ref={carouselRef}
+        className="carousel flex carousel-center bg-neutral rounded-box custom-scrollbar gap-4 overflow-x-auto "
+      >
+        {children.map((child, index) => (
+          <div
+            key={index}
+            className="2xl:min-w-[350px] xl:min-w-[350px] lg:min-w-[300px] md:min-w-[250px] min-w-[300px] bg-white  "
+          >
+            {child}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
   )
 }
 
