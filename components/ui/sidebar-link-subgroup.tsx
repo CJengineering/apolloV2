e895005cadd6cf0f87f2f4ba98821607fdb641d1
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-09-13 16:30:30 +0100

    sidebar ok

--- components/ui/sidebar-link-subgroup.tsx
+++ components/ui/sidebar-link-subgroup.tsx
@@ -15,7 +15,7 @@ export default function SidebarLinkSubgroup({
   const [linkOpen, setLinkOpen] = useState<boolean>(open);
 
   return (
-    <div className="mt-3 pr-2 md:pr-0  md:w-[205px]">
+    <div className="mt-3 pr-2 md:pr-0   md:w-[199px] ">
       <a
         className="flex   items-center justify-between text-black font-normal dark:text-white hover:text-orange-700 dark:hover:text-orange-400"
         href="#0"
@@ -25,7 +25,7 @@ export default function SidebarLinkSubgroup({
         }}
         aria-expanded={linkOpen}
       >
-        <span className="text-black dark:text-white w-36">{title}</span>
+        <span className="text-black dark:text-white ">{title}</span>
         <ChevronRightIcon
           className={`h-3 w-3 text-black dark:text-white transition-transform duration-200 ${
             linkOpen ? "rotate-90" : ""
