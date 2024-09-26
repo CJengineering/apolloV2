27ae4a3eac0199b98de4b7f95ec733c00dced7e8
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-09-26 21:08:27 +0100

    pushed last side bar

--- components/ui/sidebar-link-sub-children.tsx
+++ components/ui/sidebar-link-sub-children.tsx
@@ -15,10 +15,10 @@ export default function SidebarLinkSubChildren({
   const [linkOpen, setLinkOpen] = useState<boolean>(open);
 
   return (
-    <div className="mt-3 pr-2 w-[199px] md:pr-0 bg-blue-200 ">
-      <a
+    <div className="mt-3 pr-2 w-[px] md:pr-0  ">
+      <div
         className="flex   items-center justify-between text-black font-normal dark:text-white hover:text-orange-700 dark:hover:text-orange-400"
-        href="#0"
+ 
         onClick={(e) => {
           e.preventDefault();
           setLinkOpen(!linkOpen);
@@ -26,12 +26,15 @@ export default function SidebarLinkSubChildren({
         aria-expanded={linkOpen}
       >
         <span className="text-black dark:text-white w-36 ">{title}</span>
+        <div>
+
         <ChevronRightIcon
           className={`h-3 w-3 text-black dark:text-white transition-transform duration-200 ${
             linkOpen ? "rotate-90" : ""
           }`}
         />
-      </a>
+        </div>
+      </div>
       <div
         className={`mb-3 ml-1 pl-4 border-l border-slate-200 dark:border-slate-800 ${
           !linkOpen && "hidden"
