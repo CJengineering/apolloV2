cfac6a2a970c4dbadfe97e91707fa9262bc1b01f
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-10-15 08:04:20 +0100

    merged

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
-import { html } from 'cheerio/lib/api/manipulation'
-import { children } from 'cheerio/lib/api/traversing'
-import React from 'react'
+import { html } from "cheerio/lib/api/manipulation";
+import { children } from "cheerio/lib/api/traversing";
+import React from "react";
 
-
-export default function layout({children}: {children: React.ReactNode}) {
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <html >
-        <body>
-            {children}
-        </body>
+    <html>
+      <body>{children}</body>
     </html>
-
-  )
+  );
 }
