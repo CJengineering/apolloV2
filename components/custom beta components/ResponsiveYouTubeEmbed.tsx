b9a9f7363b5ce28e68b967608b743fd1632fecae
Nathaniel Daudrich <nathaniel@communityjameel.org>
2024-10-15 11:51:26 +0200

    performance test round 2

--- components/custom beta components/ResponsiveYouTubeEmbed.tsx
+++ components/custom beta components/ResponsiveYouTubeEmbed.tsx
@@ -1,24 +1,19 @@
-// components/ResponsiveYouTubeEmbed.tsx
-
-import React from 'react';
-
 interface ResponsiveYouTubeEmbedProps {
   embedId: string;
 }
-
 const ResponsiveYouTubeEmbed: React.FC<ResponsiveYouTubeEmbedProps> = ({ embedId }) => {
-  return React.createElement(
-    'div',
-    { className: 'w-full aspect-video' },
-    React.createElement('iframe', {
-      className: 'w-full h-full',
-      src: `https://www.youtube.com/embed/${embedId}`,
-      title: 'YouTube video player',
-      frameBorder: '0',
-      allowFullScreen: true,
-      loading: 'lazy',
-    })
+  return (
+    <div className="w-full aspect-video">
+      <iframe
+        className="w-full h-full"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        loading="lazy"
+      />
+    </div>
   );
 };
-
 export default ResponsiveYouTubeEmbed;
