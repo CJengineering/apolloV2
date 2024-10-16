39132f2dd14b70305578e502793981203369f3f5
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-10-16 14:56:37 +0100

    site maps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteUrl: 'https://www.communityjameel.org', // Replace with your actual domain
+    generateRobotsTxt: true,  // Generates a robots.txt file
+    sitemapSize: 5000,        // Split sitemaps after reaching 5000 URLs
+    changefreq: 'daily',      // Frequency of changes to URLs
+    priority: 0.7,            // Default priority for URLs
+  };
+  
