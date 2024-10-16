9525c27cd981e71511760b04dd15a6cbc9396fe8
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-10-16 12:51:41 +0100

    done

--- app/[locale]/(stories)/stories/a-cairo-cornerstone/page.tsx
+++ app/[locale]/(stories)/stories/a-cairo-cornerstone/page.tsx
@@ -5,7 +5,7 @@ import WebflowIframe from "@/components/CJ-components/components-CJ/test compone
 export default function pageJameelCenterStorie() {
   return (
     <div className="">
-    <WebflowIframe webUrl="https://www.communityjameel.org/stories/jameel-centre"></WebflowIframe>
+    <WebflowIframe webUrl="https://www.littleredheart.org/stories/jameel-centre"></WebflowIframe>
   </div>
   )
 }
