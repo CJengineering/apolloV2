b98bfd6841b3829aaead7b20dbc449d58f35d22a
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-10-16 15:04:40 +0100

    new site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    siteUrl: 'https://www.communityjameel.org', // Replace with your actual domain
-    generateRobotsTxt: true,  // Generates a robots.txt file
-    sitemapSize: 5000,        // Split sitemaps after reaching 5000 URLs
-    changefreq: 'daily',      // Frequency of changes to URLs
-    priority: 0.7,            // Default priority for URLs
+    siteUrl: 'https://www.communityjameel.org',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ['/api/*', '/server-side/*'],  // Exclude API routes or internal pages
+    robotsTxtOptions: {
+      policies: [{ userAgent: '*', allow: '/' }],
+    },
   };
   
