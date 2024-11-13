9fc88fe0ecd67dd6bccd0471fa5591dfbcaf7b56
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-11-13 16:18:46 +0100

    card changed in table row

--- components/CJ-components/components-CJ/basic components/FeatureCardAsHome.tsx
+++ components/CJ-components/components-CJ/basic components/FeatureCardAsHome.tsx
@@ -16,8 +16,10 @@ import {
   CalendarDaysIcon,
 } from "@heroicons/react/24/solid";
 const ImageContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="
-  relative h-32 w-32 lg:pb-[100%] lg:w-full group hover:cursor-pointer">
+  <div
+    className="
+  relative h-32 w-32 lg:pb-[100%] lg:w-full group hover:cursor-pointer"
+  >
     {children}
     <div className="absolute inset-0 bg-blue-950 mix-blend-screen opacity-0 transition-opacity duration-[2100ms] group-hover:opacity-100 z-10"></div>
   </div>
@@ -57,7 +59,7 @@ const iconMapping: Record<IconType, React.ComponentType> = {
   Spinout: BoltIcon,
 };
 
-export default function CardSquaredImage({
+export default function FeatureCardAsHome({
   type,
   imageUrl,
   title,
@@ -69,30 +71,33 @@ export default function CardSquaredImage({
       : null;
 
   return (
-    <div className="group ">
-      <div className="relative w-full ">
-        <div className="aspect-square overflow-hidden ">
-          
-          <Image
-            className="w-full h-full object-cover transition-transform duration-500 ease-linear group-hover:scale-105 "
-            src={imageUrl}
-            alt=""
-            width={400}
-            height={400}
-            unoptimized
-          />
+    <div className="group flex flex-row items-center lg:block ">
+      <div className="col-span-5 ">
+        <div className="relative h-32 w-32 md:h-auto md:w-full  ">
+          <div className="aspect-square overflow-hidden ">
+            <Image
+              className="w-full h-full object-cover transition-transform duration-500 ease-linear group-hover:scale-105 "
+              src={imageUrl}
+              alt=""
+              width={400}
+              height={400}
+              unoptimized
+            />
 
-          <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="absolute inset-0 bg-black opacity-50"></div>
 
-          {IconComponent && (
-            <div className="absolute left-3 top-3 h-5 w-5 text-white">
-              <IconComponent />
-            </div>
-          )}
+            {IconComponent && (
+              <div className="absolute left-3 top-3 h-5 w-5 text-white">
+                <IconComponent />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-      <div className="pt-1">
-        <p className="sans-serif text-base group-hover:underline font-medium ">{titleToUse}</p>
+      <div className="pt-1 ml- md:ml-0">
+        <p className="sans-serif text-base group-hover:underline font-medium ">
+          {titleToUse}
+        </p>
       </div>
     </div>
   );
