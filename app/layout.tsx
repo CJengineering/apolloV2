6cdf92c848ec998a6a4ea648711db93cde5bfcd5
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-11-19 13:52:32 +0100

    code refactored and build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
-import { html } from "cheerio/lib/api/manipulation";
-import { children } from "cheerio/lib/api/traversing";
+
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
