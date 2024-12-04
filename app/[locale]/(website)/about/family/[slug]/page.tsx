a4b35c034942dee7166459bc110d298654dded5b
Nathaniel Daudrich <nathaniel@communityjameel.org>
2024-12-04 09:57:11 +0100

    updates

---
+++ app/[locale]/(website)/about/family/[slug]/page.tsx
@@ -0,0 +1,246 @@
+import { FieldDataTeamProfile, Item, NewsRawFields } from "@/app/interfaces";
+import FiltredNews from "@/components/CJ-components/components-CJ/FiltredNews";
+import ContentContainer from "@/components/custom beta components/ContentContainer";
+import LanguageChanger from "@/components/custom beta components/LanguageChanger";
+import MainContainer from "@/components/custom beta components/MainContainer";
+import NewsCard from "@/components/custom beta components/NewsCard";
+import NewsSmall from "@/components/custom beta components/NewsSmall";
+import SectionBanter from "@/components/custom beta components/SectionBanter";
+import { getData } from "@/functions/api/getData";
+import filterNewsItemsByPerson from "@/functions/filters/fillterRelatedPersonNews";
+import newsMapper from "@/functions/transformers/newsMapper";
+import mapItemToNewsMainProps from "@/functions/transformers/newsTransformer";
+import teamProfileMapper from "@/functions/transformers/teamProfileMapper";
+import { getIdByDisplayName } from "@/functions/utils/findCollectionId";
+import { getDisplayName } from "next/dist/shared/lib/utils";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import { Metadata, ResolvingMetadata } from "next";
+import { customMetaDataGenerator } from "@/functions/utils/customMetadataGenerator";
+type Props = {
+  params: { slug: string; locale: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+  const locale = params.locale;
+
+  const productTest = await getData("61ee828a15a3182ecebde53f");
+  const teamMembersRaw = productTest.items;
+  const memberRaw: Item<FieldDataTeamProfile>[] = teamMembersRaw.filter(
+    (item) => item.fieldData.slug === slug
+  );
+  const seoTitleArabic = memberRaw[0].fieldData["name-arabic"]
+    ? memberRaw[0].fieldData["name-arabic"]
+    : "";
+  const seoTitleEnglish = memberRaw[0].fieldData.name
+    ? memberRaw[0].fieldData.name
+    : "";
+  const descriptionArabic = memberRaw[0].fieldData["meta-description-arabic"]
+    ? memberRaw[0].fieldData["meta-description-arabic"]
+    : "";
+  const descriptionEnglish = memberRaw[0].fieldData["meta-description"]
+    ? memberRaw[0].fieldData["meta-description"]
+    : "";
+  const name = locale === "ar" ? seoTitleArabic : seoTitleEnglish;
+  const description = locale === "ar" ? descriptionArabic : descriptionEnglish;
+  // optionally access and extend (rather than replace) parent metadata
+
+  return customMetaDataGenerator({
+    useRawTitle: true,
+    title: name,
+    description: description,
+    ogImage: memberRaw[0].fieldData.photo.url,
+  });
+}
+
+export default async function page({
+  params,
+}: {
+  params: {
+    slug: string;
+    locale: string;
+  };
+}) {
+  const data = await getData("61ee828a15a3182ecebde53f");
+  const teamMembersRaw = data.items;
+  const memberRaw = teamMembersRaw.filter(
+    (item) => item.fieldData.slug === params.slug
+  );
+  const member = teamProfileMapper(memberRaw[0]);
+  const programmeId = getIdByDisplayName("Programmes");
+  const eventId = getIdByDisplayName("Events");
+  const tagId = getIdByDisplayName("Tags");
+  const peopleId = getIdByDisplayName("People");
+  const sourceId = getIdByDisplayName("Sources");
+
+  const dataNews = await getData("61ee828a15a3185c99bde543");
+  const sourcesAll = await getData("61ee828a15a3183f55bde545");
+  const programmeAll = await getData("61ee828a15a3183d2abde540");
+  const peopleAll = await getData(peopleId);
+  const tagAll = await getData(tagId);
+  const eventAll = await getData(eventId);
+
+  const personId = memberRaw[0].id;
+  const filteredNewsItems = dataNews.items.filter((item) =>
+    item.fieldData["related-team-members"]?.includes(personId)
+  );
+
+  const relatedNews = filteredNewsItems.map((item) =>
+    mapItemToNewsMainProps(item, sourcesAll.items, programmeAll.items)
+  );
+
+  const relatedNewsToTeamMember = filteredNewsItems.map((item) =>
+    newsMapper(
+      item,
+      programmeAll.items,
+      peopleAll.items,
+      sourcesAll.items,
+      tagAll.items,
+      eventAll.items
+    )
+  );
+
+  const hiresPhoto = member.photoHiRes;
+  const memberName = params.locale === "ar" ? member.nameArabic : member.name;
+  const memberPosition =
+    params.locale === "ar" ? member.postionArabic : member.position;
+  const memberBiography =
+    params.locale === "ar"
+      ? member.biographyArabic
+      : member.paragraphDescription;
+
+  return (
+    <>
+      <div className={params.locale === "ar" ? "rtl" : ""}>
+        <div className="pb-6 pt-16 lg:pt-10 flex flex-col">
+          <div className="flex items-center gap-x-3 pb-3 lg:pb-6">
+            <div>
+              <p
+                className={`underline hover:text-blue-800 ${
+                  params.locale === "ar" ? "sans-serif-ar" : "sans-serif"
+                }`}
+              >
+                <a href="/about/team">
+                  {params.locale === "ar" ? "فريق" : "Team"}
+                </a>
+              </p>
+            </div>
+            <div>
+              <p
+                className={`${
+                  params.locale === "ar" ? "sans-serif-ar" : "sans-serif"
+                }`}
+              >
+                <ChevronRightIcon className="w-4 h-4 text-gray-500" />
+              </p>
+            </div>
+            <div>
+              <p
+                className={`sans-serif ${
+                  params.locale === "ar" ? "sans-serif-ar" : ""
+                }`}
+              >
+                {memberName}
+              </p>
+            </div>
+          </div>
+          {params.locale === "ar" ? (
+            <div className="flex justify-between">
+              <div className="pb-2 ml-4">
+                <h1 className="text-3xl sm:text-4xl lg:text-6xl sans-serif-ar">
+                  {memberName}
+                </h1>
+              </div>
+              <div className="mt-2">
+                <LanguageChanger></LanguageChanger>
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-between">
+              <div className="mr-4 pb-2">
+                <h1 className="text-3xl sm:text-4xl lg:text-6xl sans-serif lg:-ml-[1px]">
+                  {memberName}
+                </h1>
+              </div>
+              <div className="mt-2">
+                <LanguageChanger></LanguageChanger>
+              </div>
+            </div>
+          )}
+
+          <div className="w-full">
+            <p
+              className={`sans-serif text-lg font-normal ${
+                params.locale === "ar" ? "sans-serif-ar" : ""
+              }`}
+            >
+              {memberPosition}
+            </p>
+          </div>
+        </div>
+
+        {/* Flex container to align the image with text wrapping */}
+        <div className="flex-col">
+          <div
+            className={`w-full lg:w-[330px] mr-6 mb-6 ${
+              params.locale === "ar" ? "mr-0 lg:w-[330px] lg:mr-0" : ""
+            }`}
+          >
+            <img
+              src={member.imageUrl}
+              alt={member.altTextImage}
+              className="w-full h-auto object-cover"
+            />
+            <p
+              className={`text-xs font-normal uppercase hover:underline hover:text-blue-900 cursor-pointer pt-1 ${
+                params.locale === "ar" ? "sans-serif-ar" : "mono"
+              }`}
+            >
+              <a href={hiresPhoto}>
+                {params.locale === "ar"
+                  ? "تنزيل صورة عالية الدقة"
+                  : "Download high-resolution photograph"}
+              </a>
+            </p>
+          </div>
+          {/* Biography text with prose for rich text formatting */}
+          <div
+            className={`pl-0 prose prose-2xl dark:prose-dark ${
+              params.locale === "ar" ? "sans-serif-ar" : "sans-serif"
+            }`}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html: memberBiography ? memberBiography : "",
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      {relatedNewsToTeamMember.length > 0 && (
+        <>
+          <div className="flex flex-col pt-3 pb-6">
+            <div className="w-full h-px bg-slate-200 dark:bg-slate-700"></div>{" "}
+            {/* Separation Bar */}
+          </div>
+          <div className="pb-6">
+            <h2 className="header-section pb-3">Related content</h2>
+          </div>
+        </>
+      )}
+      <div className="w-full lg:w-2/3">
+      <div className="grid grid-cols-1 gap-3">
+        {relatedNewsToTeamMember.map((item) => (
+          <NewsCard key={item.slug} content={item} locale={params.locale} />
+        ))}
+      </div>
+      </div>
+    </>
+  );
+}
--- app/[locale]/(website)/about/family/page.tsx
+++ app/[locale]/(website)/about/family/page.tsx
@@ -6,7 +6,6 @@ import { customMetaDataGenerator } from "@/functions/utils/customMetadataGenerat
 import SectionDivider from "@/components/components V2/generic/section-divider";
 import { TeamMember } from "@/app/interfaces";
 import Image from 'next/image';
-
 export const metadata: Metadata = customMetaDataGenerator({
   useRawTitle: true,
   title: "Team",
@@ -14,53 +13,15 @@ export const metadata: Metadata = customMetaDataGenerator({
     "Community Jameel supports a community of scientists, humanitarians, technologists and creatives. Working together through centres, funds, scholarships and projects, we are advancing science to help communities thrive in a rapidly changing world.",
   ogType: "website",
   ogImage:
-    "https://cdn.prod.website-files.com/612cdb8a4fac760705621df5/674f30029a0622fd75426e6d_TEAM_PHOTO_HERO.webp",
+    "https://cdn.prod.website-files.com/612cdb8a4fac760705621df5/674f2b2d1bcc7279c4a8abe9_FAMILY_OPEN_GRAPH.webp",
   twitterCard: "summary_large_image",
   keywords: ["Community Jameel", "Jameel", "Community", "Team"],
 });
 
-function SectionCoreTeam({ team }: { team: TeamMember[] }) {
-  return (
-    <section className="">
-      <h2 className="header-section py-6">Core</h2>
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
-        {team.map((member) => (
-          <PersonalCard
-            key={member.order}
-            author={member}
-            socialPlatforms={[]}
-          ></PersonalCard>
-        ))}
-      </div>
-    </section>
-  );
-}
-
-function SectionCommitteeTeam({
-  advisoryCommittee,
-}: {
-  advisoryCommittee: TeamMember[];
-}) {
-  return (
-    <section className="">
-      <h2 className="header-section pb-6">Advisory Committee</h2>
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {advisoryCommittee.map((member) => (
-          <PersonalCard
-            key={member.order}
-            author={member}
-            socialPlatforms={["Twitter", "Linkedin"]}
-          ></PersonalCard>
-        ))}
-      </div>
-    </section>
-  );
-}
-
 function SectionFamilyTeam({ leadership }: { leadership: TeamMember[] }) {
   return (
     <section>
-      <h2 className="header-section pb-6">Jameel Family</h2>
+       
       <p className="sans-serif font-normal text-base mb-6">
         Check out a selection of{" "}
         <a
@@ -71,7 +32,7 @@ function SectionFamilyTeam({ leadership }: { leadership: TeamMember[] }) {
         </a>{" "}
         of the Jameel family from across the history of Community Jameel.
       </p>
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
         {leadership.map((member) => (
           <PersonalCard
             key={member.order}
@@ -125,10 +86,9 @@ export default async function PeopleContent({
 
   return (
     <>
-      <h1 className="header-page pb-3">Team</h1>
-      <SectionCoreTeam team={team} />
-      <SectionDivider />
-      <SectionCommitteeTeam advisoryCommittee={advisoryCommittee} />
+        <div className="pt-[53px] sm:pt-[68px] md:pt-[36px] lg:pt-2"><Image src="https://cdn.prod.website-files.com/612cdb8a4fac760705621df5/674f2b2d2fd2c4071a040b12_FAMILY_HERO_IMAGE.webp" alt="Jameel Family" width={940} height={540} /></div>
+      <h1 className="sans-serif font-bold text-4xl lg:text-6xl pt-3 pb-3 lg:pt-6 ">Jameel Family</h1>
+        <SectionFamilyTeam leadership={leadership}></SectionFamilyTeam>
       <div className="pb-24"></div>
     </>
   );
