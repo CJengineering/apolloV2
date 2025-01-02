b6625bb4dec2d419f063220a3b027a5c8c8c4a37
Nathaniel Daudrich <nathaniel@communityjameel.org>
2025-01-02 16:58:19 +0100

    update to brand page and reupload of logos zip

--- app/[locale]/(website)/about/brand/page.tsx
+++ app/[locale]/(website)/about/brand/page.tsx
@@ -30,13 +30,13 @@ const ColourTable: FC = () => {
       <table className="w-full lg:w-2/3 table-auto border-collapse">
         <thead>
           <tr>
-            <th className="py-3 border-b border-t border-gray-200 text-left sans-serif text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="py-3 border-b border-t border-gray-200 dark:border-gray-400 text-left sans-serif text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
               Colour
             </th>
-            <th className="pl-3 py-3 border-b border-t border-gray-200 text-left sans-serif text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="pl-3 py-3 border-b border-t border-gray-200  dark:border-gray-400 text-left sans-serif text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
               Hex
             </th>
-            <th className="pl-3 py-3 border-b border-t border-gray-200 text-left sans-serif text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="pl-3 py-3 border-b border-t border-gray-200  dark:border-gray-400 text-left sans-serif text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
               RGB
             </th>
           </tr>
@@ -80,21 +80,11 @@ export default async function Page() {
           </div>
           <div><Logo></Logo></div>
           <br />
-          <p className="text-base sans-serif w-full lg:w-2/3">
-            Our logo was inspired by our founder and chairman, Mohammed Jameel KBE. The heart-shaped Arabic calligram reads <span className="font-bold">لأننا نحبكم</span>, which translates to English as <span className="font-bold">Because we love you</span>.
-            <br /><br />
-            This message of love underpins the Jameel family’s commitment to humanity and the community we serve.
-            <br /><br />
-            You can download our logo in different colours and formats{' '}
-            <a
-              href="/images/cj_logo/COMMUNITY_JAMEEL_LOGOS_ALL.zip"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline hover:text-blue-800"
-            >
-              here
-            </a>.
-          </p>
+          <div className="prose prose-xl leading-normal dark:prose-dark">
+          <p>Our logo was inspired by our founder and chairman, Mohammed Jameel KBE. The heart-shaped Arabic calligram reads <span className="font-bold">لأننا نحبكم</span>, which translates to English as <span className="font-bold">Because we love you</span>.</p>
+          <p>This message of love underpins the Jameel family’s commitment to humanity and the community we serve.</p>
+          <p>You can download our logo in different colours and formats <a href="/images/cj_logo/COMMUNITY_JAMEEL_LOGOS.zip" target="_blank" rel="noopener noreferrer">here</a>.</p>
+          </div>
         </div>
 
         {/* Fonts Section */}
@@ -102,11 +92,10 @@ export default async function Page() {
           <div className="pb-3">
             <h2 className="header-section">Fonts</h2>
           </div>
-          <p className="text-base sans-serif w-full lg:w-2/3">
-            For digital and print communications, we use Arial. It is clear and modern and, as a standard font, is available on PCs and Macs and translates well across formats.
-            <br /><br />
-            If you want to make an implicit connection to our brand, use <span className="costa font-bold">Costa Bold</span>. This is the font in our logo and is synonymous with our identity. It should be used sparingly and principally for headings.
-          </p>
+          <div className="prose prose-xl leading-normal dark:prose-dark">
+          <p>For digital and print communications, we use Arial. It is clear and modern and, as a standard font, is available on PCs and Macs and translates well across formats.</p>
+          <p>If you want to make an implicit connection to our brand, use <span className="costa font-bold">Costa Bold</span>. This is the font in our logo and is synonymous with our identity. It should be used sparingly and principally for headings.</p>
+          </div>
         </div>
 
         {/* Colours Section */}
@@ -114,11 +103,10 @@ export default async function Page() {
           <div className="pb-3">
             <h2 className="header-section">Colours</h2>
           </div>
-          <p className="text-base sans-serif w-full lg:w-2/3">
-          Our main colour is a <span className="font-bold text-[#d02146]">dark red</span> [HEX: #d02146 | rgb(209, 33, 70)] to reflect the heart that underpins our identity.
-          <br /><br />
-          We also use colours that evoke the long dawns and dusks on the shores of the Red Sea in Jeddah.
-          </p>
+          <div className="prose prose-xl leading-normal dark:prose-dark">
+          <p>Our main colour is a <span className="font-bold text-[#d02146]">dark red</span> to reflect the heart that underpins our identity.</p>
+          <p>We also use colours that evoke the long dawns and dusks on the shores of the Red Sea in Jeddah.</p>
+          </div>
         </div>
 
         
